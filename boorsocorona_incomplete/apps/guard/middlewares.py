921285f6f842582019168085beb23a8b9d02fd5b
DJS <samanzandh84@gmail.com>
2023-03-17 19:30:02 +0330

    Boors o Corona is incomplete (the last test fails)

--- boorsocorona_incomplete/apps/guard/middlewares.py
+++ boorsocorona_incomplete/apps/guard/middlewares.py
@@ -1,8 +1,5 @@
-from django.http import JsonResponse, HttpResponseForbidden, HttpResponse
-
 from django.core.cache import cache
-from django.urls import resolve
-from datetime import datetime
+from django.http import HttpResponseForbidden, HttpResponse
 from django.utils import timezone
 from rest_framework import status
 
@@ -11,12 +8,12 @@ from .models import BlockedIp, SecurityConfig, ViewDetail
 from .utils import get_client_ip
 
 
-class DDOSResponse(HttpResponse):
-    status_code = 429
+class HttpTooManyRequests(HttpResponse):
+    status_code = status.HTTP_429_TOO_MANY_REQUESTS
 
 
 class BlockIpMiddleware(object):
-    maximum_rps = 1
+    maximum_rps = 4
     timeout = 60
 
     def __init__(self, get_response):
@@ -24,8 +21,8 @@ class BlockIpMiddleware(object):
 
     def __call__(self, request):
         self.path = request.path
-        self.user_ip = get_client_ip(request)
-        LIMITED_VIEWS = SecurityConfig.objects.last().views.values_list(
+        self.user_ip = get_client_ip(request)        
+        self.VIEWS = SecurityConfig.objects.last().views.values_list(
             'path',
             flat=True
         )
@@ -36,7 +33,7 @@ class BlockIpMiddleware(object):
 
         self._handle_cache()
         if not self.validate_request_per_second():
-            return DDOSResponse()
+            return HttpTooManyRequests()
         
         return self.get_response(request)
 
@@ -47,24 +44,23 @@ class BlockIpMiddleware(object):
         self.req_key = f"{self.user_ip}:req"
         self.time_key = f"{self.user_ip}:time"
         
-        if not cache.get(self.req_key):
+        if not cache.has_key(self.req_key):
             cache.set(self.req_key, 1, self.timeout)
         else:
             cache.incr(self.req_key, 1)
             
-        if not cache.get(self.time_key):
-            cache.set(self.time_key, timezone.now().isoformat(), timeout=60)
-        
-
+        if not cache.has_key(self.time_key):
+            cache.set(self.time_key, int(timezone.now().timestamp()), timeout=60)
+            
     def validate_request_per_second(self, *args, **kwargs):
         reqs = int(cache.get(self.req_key))
-        time = cache.get(self.time_key)
-        time_delta = (timezone.now() - datetime.fromisoformat(time)).microseconds
-        rps = reqs/(time_delta*10**6)
-            
-        if rps > self.maximum_rps:
+        timestamp = cache.get(self.time_key)
+        time_delta = int(timezone.now().timestamp()) - timestamp
+        rps = reqs/time_delta if time_delta!=0 else 0
+        
+        if rps > self.maximum_rps and self.path in self.VIEWS:
             view_object = ViewDetail.objects.get(path=self.path)
-            BlockedIp.objects.create(ip=self.ip,
+            BlockedIp.objects.create(ip=self.user_ip,
                                      view=view_object,
                                      rps=int(rps))
             return False
